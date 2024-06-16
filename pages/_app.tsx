64df121d90a73757db4c9a09978f2f0b0bdcee25
Pourya Soleimani <pouryasoleimani93@gmail.com>
2024-06-16 05:02:13 -0700

    FONTS

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,17 +1,25 @@
+/* eslint-disable prettier/prettier */
+/* eslint-disable react/jsx-sort-props */
 import type { AppProps } from "next/app";
 
 import { NextUIProvider } from "@nextui-org/react";
 import { ThemeProvider as NextThemesProvider } from "next-themes";
 import { useRouter } from "next/router";
+import { Noto_Sans_Arabic } from 'next/font/google'
 
 import { fontSans, fontMono } from "@/config/fonts";
 import "@/styles/globals.css";
+
+const inter = Noto_Sans_Arabic({
+subsets: ['arabic'],
+  weight: "700"
+})
 
 export default function App({ Component, pageProps }: AppProps) {
   const router = useRouter();
 
   return (
-    <NextUIProvider navigate={router.push}>
+    <NextUIProvider navigate={router.push} className={inter.className}>
       <NextThemesProvider>
         <Component {...pageProps} />
       </NextThemesProvider>

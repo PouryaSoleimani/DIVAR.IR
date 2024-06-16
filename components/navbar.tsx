897ec4e7c1c6b6dd327c8a81bf39fce02e4cba69
Pourya Soleimani <pouryasoleimani93@gmail.com>
2024-06-16 03:49:33 -0700

    NAVBAR

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -24,7 +24,7 @@ export const Navbar = () => {
       <NavbarContent className="basis-1/5 sm:basis-full" justify="start">
         <NavbarBrand className="gap-3 max-w-fit">
           <NextLink className="flex justify-start items-center gap-1" href="/">
-            <Logo />
+            <Logo />  
           </NextLink>
         </NavbarBrand>
 

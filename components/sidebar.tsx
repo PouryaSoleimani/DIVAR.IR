a7790f46128178d720cf65fb2dd932ee4a5b9999
Pourya Soleimani <pouryasoleimani93@gmail.com>
2024-06-16 06:04:40 -0700

    SIDEBAR

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -15,20 +15,25 @@ import { PiSuitcaseBold } from "react-icons/pi";
 
 export const Sidebar = () => {
   return (
-    <div id="sideBarCategories" className='tracking-tighter h-full w-fit text-[14px] border-b border-zinc-300 flex items-start justify-start pr-4 text-zinc-500 leading-5 pl-10 '>
-      <ul className='flex flex-col space-y-5 font-thin tracking-tighter'>
-        <h2 className='text-zinc-400 text-[12px] text-end px-2'>دسته ها</h2>
-        <li className='flex flex-row-reverse items-center gap-2 cursor-pointer text-[10px] sm:text-[12px] leading-4 font-light hover:bg-zinc-200 px-2 py-1 rounded-lg'><TbBuildingEstate className='w-6 h-6' />املاک </li>
-        <li className='flex flex-row-reverse items-center gap-2 cursor-pointer text-[10px] sm:text-[12px] leading-4 font-light hover:bg-zinc-200 px-2 py-1 rounded-lg'><IoCarSportOutline className='w-6 h-6' />وسایل نقلیه</li>
-        <li className='flex flex-row-reverse items-center gap-2 cursor-pointer text-[10px] sm:text-[12px] leading-4 font-light hover:bg-zinc-200 px-2 py-1 rounded-lg'><HiOutlineDevicePhoneMobile className='w-6 h-6' />کالای دیجیتال</li>
-        <li className='flex flex-row-reverse items-center gap-2 cursor-pointer text-[10px] sm:text-[12px] leading-4 font-light hover:bg-zinc-200 px-2 py-1 rounded-lg'><LuLamp className='w-6 h-6' />خانه و آشپزحانه</li>
-        <li className='flex flex-row-reverse items-center gap-2 cursor-pointer text-[10px] sm:text-[12px] leading-4 font-light hover:bg-zinc-200 px-2 py-1 rounded-lg'><PiPaintBrushBroadBold className='w-6 h-6' />خدمات</li>
-        <li className='flex flex-row-reverse items-center gap-2 cursor-pointer text-[10px] sm:text-[12px] leading-4 font-light hover:bg-zinc-200 px-2 py-1 rounded-lg'><BsSmartwatch className='w-6 h-6' />وسایل شخصی</li>
-        <li className='flex flex-row-reverse items-center gap-2 cursor-pointer text-[10px] sm:text-[12px] leading-4 font-light hover:bg-zinc-200 px-2 py-1 rounded-lg'><LuDices className='w-6 h-6' />سرگرمی و فراغت</li>
-        <li className='flex flex-row-reverse items-center gap-2 cursor-pointer text-[10px] sm:text-[12px] leading-4 font-light hover:bg-zinc-200 px-2 py-1 rounded-lg'><GoPeople className='w-6 h-6' />اجتماعی</li>
-        <li className='flex flex-row-reverse items-center gap-2 cursor-pointer text-[12px] sm:[12px] leading-4 font-light hover:bg-zinc-200 px-2 py-1 rounded-lg'><PiOfficeChairBold className='w-6 h-6' />تجهیزات و صنعتی</li>
-        <li className='flex flex-row-reverse items-center gap-2 cursor-pointer text-[10px] sm:text-[12px] leading-4 font-light hover:bg-zinc-200 px-2 py-1 rounded-lg'><PiSuitcaseBold className='w-6 h-6' />استخدام و کاریابی </li>
-      </ul>
-    </div>
+    <section className='flex flex-col items-start justify-start'>
+      <div id="sideBarCategories" className='tracking-tighter h-fit pb-6 w-fit text-[14px] border-b border-zinc-300 flex items-start justify-start pr-4 text-zinc-500 leading-5 pl-16 '>
+        <ul className='flex flex-col space-y-5 font-thin tracking-tighter'>
+          <h2 className='text-zinc-400 text-[12px] text-end px-2'>دسته ها</h2>
+          <li className='flex flex-row-reverse items-center gap-2 cursor-pointer text-[10px] sm:text-[12px] leading-4 font-light hover:bg-zinc-200 px-2 py-1 rounded-lg'><TbBuildingEstate className='w-6 h-6' />املاک </li>
+          <li className='flex flex-row-reverse items-center gap-2 cursor-pointer text-[10px] sm:text-[12px] leading-4 font-light hover:bg-zinc-200 px-2 py-1 rounded-lg'><IoCarSportOutline className='w-6 h-6' />وسایل نقلیه</li>
+          <li className='flex flex-row-reverse items-center gap-2 cursor-pointer text-[10px] sm:text-[12px] leading-4 font-light hover:bg-zinc-200 px-2 py-1 rounded-lg'><HiOutlineDevicePhoneMobile className='w-6 h-6' />کالای دیجیتال</li>
+          <li className='flex flex-row-reverse items-center gap-2 cursor-pointer text-[10px] sm:text-[12px] leading-4 font-light hover:bg-zinc-200 px-2 py-1 rounded-lg'><LuLamp className='w-6 h-6' />خانه و آشپزحانه</li>
+          <li className='flex flex-row-reverse items-center gap-2 cursor-pointer text-[10px] sm:text-[12px] leading-4 font-light hover:bg-zinc-200 px-2 py-1 rounded-lg'><PiPaintBrushBroadBold className='w-6 h-6' />خدمات</li>
+          <li className='flex flex-row-reverse items-center gap-2 cursor-pointer text-[10px] sm:text-[12px] leading-4 font-light hover:bg-zinc-200 px-2 py-1 rounded-lg'><BsSmartwatch className='w-6 h-6' />وسایل شخصی</li>
+          <li className='flex flex-row-reverse items-center gap-2 cursor-pointer text-[10px] sm:text-[12px] leading-4 font-light hover:bg-zinc-200 px-2 py-1 rounded-lg'><LuDices className='w-6 h-6' />سرگرمی و فراغت</li>
+          <li className='flex flex-row-reverse items-center gap-2 cursor-pointer text-[10px] sm:text-[12px] leading-4 font-light hover:bg-zinc-200 px-2 py-1 rounded-lg'><GoPeople className='w-6 h-6' />اجتماعی</li>
+          <li className='flex flex-row-reverse items-center gap-2 cursor-pointer text-[12px] sm:[12px] leading-4 font-light hover:bg-zinc-200 px-2 py-1 rounded-lg'><PiOfficeChairBold className='w-6 h-6' />تجهیزات و صنعتی</li>
+          <li className='flex flex-row-reverse items-center gap-2 cursor-pointer text-[10px] sm:text-[12px] leading-4 font-light hover:bg-zinc-200 px-2 py-1 rounded-lg'><PiSuitcaseBold className='w-6 h-6' />استخدام و کاریابی </li>
+        </ul>
+      </div>
+      <div id="sideBarPlace">
+        
+      </div>
+    </section>
   )
 }

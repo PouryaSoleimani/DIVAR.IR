8953f6735372bb28356131b3b0c3b36981db9197
Pourya Soleimani <pouryasoleimani93@gmail.com>
2024-06-16 04:38:02 -0700

    SIDEBAR

---
+++ components/sidebar.tsx
@@ -0,0 +1,35 @@
+/* eslint-disable prettier/prettier */
+import React from 'react'
+import { TbBuildingEstate } from "react-icons/tb";
+import { IoCarSportOutline } from "react-icons/io5";
+import { HiOutlineDevicePhoneMobile } from "react-icons/hi2";
+import { LuLamp } from "react-icons/lu";
+import { PiPaintBrushBroadBold } from "react-icons/pi";
+import { BsSmartwatch } from "react-icons/bs";
+import { LuDices } from "react-icons/lu";
+import { GoPeople } from "react-icons/go";
+import { PiOfficeChairBold } from "react-icons/pi";
+import { PiSuitcaseBold } from "react-icons/pi";
+import { LuChevronDown } from "react-icons/lu";
+
+export const Sidebar = () => {
+  return (
+    <div>
+      <div id="sideBarCategories" className='tracking-tighter relative top-0 right-0 h-full text-[14px] border-b border-zinc-300 mb-6 pb-6 flex items-center justify-start pr-8  text-zinc-800 leading-5'>
+        <ul className='flex flex-col space-y-5 font-thin tracking-tighter'>
+          <h2 className='text-zinc-700 text-[12px]'>دسته ها</h2>
+          <li className='flex items-center gap-2 cursor-pointer text-[10px] sm:text-[12px] leading-4 font-light hover:text-zinc-900'><TbBuildingEstate />املاک </li>
+          <li className='flex items-center gap-2 cursor-pointer text-[10px] sm:text-[12px] leading-4 font-light hover:text-zinc-900'><IoCarSportOutline />وسایل نقلیه</li>
+          <li className='flex items-center gap-2 cursor-pointer text-[10px] sm:text-[12px] leading-4 font-light hover:text-zinc-900'><HiOutlineDevicePhoneMobile />کالای دیجیتال</li>
+          <li className='flex items-center gap-2 cursor-pointer text-[10px] sm:text-[12px] leading-4 font-light hover:text-zinc-900'><LuLamp />خانه و آشپزحانه</li>
+          <li className='flex items-center gap-2 cursor-pointer text-[10px] sm:text-[12px] leading-4 font-light hover:text-zinc-900'><PiPaintBrushBroadBold />خدمات</li>
+          <li className='flex items-center gap-2 cursor-pointer text-[10px] sm:text-[12px] leading-4 font-light hover:text-zinc-900'><BsSmartwatch />وسایل شخصی</li>
+          <li className='flex items-center gap-2 cursor-pointer text-[10px] sm:text-[12px] leading-4 font-light hover:text-zinc-900'><LuDices />سرگرمی و فراغت</li>
+          <li className='flex items-center gap-2 cursor-pointer text-[10px] sm:text-[12px] leading-4 font-light hover:text-zinc-900'><GoPeople />اجتماعی</li>
+          <li className='flex items-center gap-2 cursor-pointer text-[10px] sm:[12px] leading-4 font-light hover:text-zinc-900'><PiOfficeChairBold />تجهیزات و صنعتی</li>
+          <li className='flex items-center gap-2 cursor-pointer text-[10px] sm:text-[12px] leading-4 font-light hover:text-zinc-900'><PiSuitcaseBold />استخدام و کاریابی </li>
+        </ul>
+      </div>
+    </div>
+  )
+}
---
+++ pages/test/index.tsx
@@ -0,0 +1,15 @@
+/* eslint-disable prettier/prettier */
+/* eslint-disable import/order */
+/* eslint-disable prettier/prettier */
+import { Sidebar } from '@/components/sidebar'
+import React from 'react'
+
+function index() {
+  return (
+    <div >
+      <Sidebar />
+    </div>
+  )
+}
+
+export default index

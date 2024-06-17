5280bdfd2dcade8e67cb1cd3d7900b6701a9b8bb
Pourya Soleimani <pouryasoleimani93@gmail.com>
2024-06-17 12:38:32 -0700

    SINGLE ITEM

--- components/singleItem.tsx
+++ components/singleItem.tsx
@@ -5,13 +5,13 @@ import React from 'react'
 
 const SingleItem = () => {
   return (
-    <div className='border w-fit h-fit py-4 px-3 rounded-md mt-10 flex gap-x-4 cursor-pointer hover:border-black duration-300'>
+    <div className='border-2 w-fit h-fit py-4 px-3 rounded-md mt-10 flex gap-x-4 cursor-pointer hover:border-zinc-600 duration-300'>
       <Image src='/ac35c186-cdc1-4d65-b520-d9288a0db8c9.webp' width={100} height={50} alt='Single-item-img' />
       <div className='flex flex-col items-end justify-between'>
         <h1>دوچرخه 24 گالانت</h1>
         <span className='flex flex-col items-end justify-around space-y-2'>
           <p className='text-sm text-zinc-400'>نو</p>
-          <p className='text-sm text-zinc-500 rtl font-normal'>تومان7/500/000   </p>
+          <p className='text-sm text-zinc-500 rtl flex flex-row gap-x-1 leading-3'>تومان<span>7/500/000</span>   </p>
           <p className='text-sm text-zinc-500'>لحظاتی پیش در کیانمهر</p>
         </span>
       </div>

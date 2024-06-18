036ea9b103d008b871f09cc416f7228c3b831a14
Pourya Soleimani <pouryasoleimani93@gmail.com>
2024-06-18 03:17:24 -0700

    SERVER

--- components/singleItem.tsx
+++ components/singleItem.tsx
@@ -3,18 +3,19 @@
 import { Image } from '@nextui-org/react'
 import NextImage from "next/image";
 import React from 'react'
+type SingleItemType = { id: number, title: string, location: string, img: string, price: number, status: string }
 
-const SingleItem = ({}) => {
-
+const SingleItem = (props: SingleItemType) => {
+  console.log(props)
   return (
     <div className='border-2 w-fit h-fit py-4 px-3 rounded-md mt-10 flex gap-x-4 cursor-pointer hover:border-zinc-600 duration-300'>
-      <Image as={NextImage} isZoomed isBlurred radius='none' src='/ac35c186-cdc1-4d65-b520-d9288a0db8c9.webp' width={100} height={50} alt='Single-item-img' />
+      <Image as={NextImage} isZoomed isBlurred radius='none' src={props.img} width={100} height={50} alt='Single-item-img' />
       <div className='flex flex-col items-end justify-between'>
-        <h1>دوچرخه 24 گالانت</h1>
+        <h1>{props.title}</h1>
         <span className='flex flex-col items-end justify-around space-y-2'>
-          <p className='text-sm text-zinc-500'>نو</p>
-          <p className='text-sm text-zinc-500 rtl flex flex-row gap-x-1 leading-3'>تومان<span>7/500/000</span>   </p>
-          <p className='text-sm text-zinc-500'>لحظاتی پیش در کیانمهر</p>
+          <p className='text-sm text-zinc-500'>{props.status}</p>
+          <p className='text-sm text-zinc-500 rtl flex flex-row gap-x-1 leading-3'>تومان<span>{props.price.toLocaleString()}</span>   </p>
+          <p className='text-sm text-zinc-500'>لحظاتی پیش در <span>{props.location}</span></p>
         </span>
       </div>
     </div>
--- pages/test/index.tsx
+++ pages/test/index.tsx
@@ -1,17 +1,31 @@
 /* eslint-disable prettier/prettier */
 /* eslint-disable import/order */
 /* eslint-disable prettier/prettier */
-import { Sidebar } from '@/components/sidebar'
-import SingleItem from '@/components/singleItem'
 import React from 'react'
+import axios from 'axios'
+import SingleItem from '@/components/singleItem'
+import { Sidebar } from '@/components/sidebar'
+import { GetStaticProps } from 'next'
 
-function index() {
+
+interface DataType { response: { id: number, title: string, price: number, views: number, status: string, location: string, img: string } }
+
+function TestPage({ response }: DataType) {
+
   return (
     <div className='flex flex-row-reverse'>
       <Sidebar />
-      <SingleItem />
+      <SingleItem {...response} />
     </div>
   )
 }
+export const getStaticProps: GetStaticProps = async (ctx) => {
+  const request = axios.get('http://localhost:5000/products/1')
+  const response = (await request).data
 
-export default index
+  return {
+    props: { response },
+  }
+}
+
+export default TestPage
